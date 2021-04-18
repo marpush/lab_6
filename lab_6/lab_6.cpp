34a216aabf7c07ab3f6a16208edaa27e29e9c359
Марьяна <@DESKTOP-8H8GHND>
2021-04-18 16:10:05 +0600

    Добавьте файлы проекта.

---
+++ lab_6/lab_6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+struct Сonstruction {
+	Сonstruction* midline;
+	Сonstruction* left;
+	Сonstruction* right;
+	int value;
+};
+
+Сonstruction* MakeElements(int N) {
+	if (N == 0) {
+		return NULL;
+	}
+	Сonstruction* p;
+	p = new Сonstruction;
+	Сonstruction* p0 = p-> midline = new Сonstruction;
+	p0->value = rand();
+	N--;
+	if (N == 0) {
+		p->left = p->right = NULL;
+	}
+	Сonstruction* p1 = p->left = new Сonstruction;
+	Сonstruction* p2 = p->right = new Сonstruction;
+	p1->right = p2;
+	p2->left = p1;
+	p1->value = rand();
+	p2->value = rand();
+	p1->left = p2->right = MakeElements(N-2);
+	return p;
+}
+
+int main()
+{
+	Сonstruction* root = MakeElements(3);
+	cout << root->midline->value << endl;
+	cout << root->left->value << endl;
+	cout << root->right->value << endl;
+	return 0;
+}
